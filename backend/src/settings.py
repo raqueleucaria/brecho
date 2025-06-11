d29b48ca5f4b4f8461689058669fdcadcbf8730f
raqueleucaria <raqueleucaria@gmail.com>
2025-06-11 11:22:56 -0300

    fix(back settings): allow extra environment variables in Settings

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,8 +1,11 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='../.env', env_file_encoding='utf-8'
+        env_file='../.env',
+        env_file_encoding='utf-8',
+        extra='allow'
     )
 
     MYSQL_USERNAME: str
@@ -14,6 +17,6 @@ class Settings(BaseSettings):
     @property
     def DATABASE_URL(self) -> str:
         return (
-            f"mysql+pymysql://{self.MYSQL_USERNAME}:{self.MYSQL_PASSWORD}"
-            f"@{self.MYSQL_HOST}:{self.MYSQL_LOCAL_PORT}/{self.MYSQL_DATABASE}"
+            f'mysql+pymysql://{self.MYSQL_USERNAME}:{self.MYSQL_PASSWORD}'
+            f'@{self.MYSQL_HOST}:{self.MYSQL_LOCAL_PORT}/{self.MYSQL_DATABASE}'
         )
