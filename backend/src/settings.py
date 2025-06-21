f1799350c03a4d02af89603fc75388d4fd576736
raqueleucaria <raqueleucaria@gmail.com>
2025-06-21 11:58:07 -0300

    fix(infra): adjustment for docker configuration and best practices with DATABASE_URL

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -3,18 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(
-        env_file='../.env', env_file_encoding='utf-8', extra='allow'
+        env_file='.env', env_file_encoding='utf-8', extra='allow'
     )
 
-    MYSQL_USERNAME: str
-    MYSQL_PASSWORD: str
-    MYSQL_HOST: str
-    MYSQL_LOCAL_PORT: str
-    MYSQL_DATABASE: str
-
-    @property
-    def DATABASE_URL(self) -> str:
-        return (
-            f'mysql+pymysql://{self.MYSQL_USERNAME}:{self.MYSQL_PASSWORD}'
-            f'@{self.MYSQL_HOST}:{self.MYSQL_LOCAL_PORT}/{self.MYSQL_DATABASE}'
-        )
+    DATABASE_URL: str
