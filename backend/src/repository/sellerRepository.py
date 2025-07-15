88e062a01fb14fd90e8cc76ac33f3152da6e0994
raqueleucaria <raqueleucaria@gmail.com>
2025-07-15 13:01:18 -0300

    refatorando seller

--- backend/src/repository/sellerRepository.py
+++ backend/src/repository/sellerRepository.py
@@ -6,38 +6,45 @@ from src.model.seller import Seller
 
 class SellerRepository:
     @staticmethod
-    async def get_seller_by_id(session: AsyncSession, seller_id: int):
-        return await session.scalar(
-            select(Seller).where(Seller.seller_id == seller_id)
-        )
-
-    @staticmethod
     async def get_seller_by_user_id(session: AsyncSession, user_id: int):
         return await session.scalar(
             select(Seller).where(Seller.user_id == user_id)
         )
 
     @staticmethod
-    async def get_sellers(session: AsyncSession):
-        result = await session.scalars(select(Seller))
-        return result.all()
+    async def create_seller(session: AsyncSession, seller: Seller):
+        session.add(seller)
+        await session.commit()
+        await session.refresh(seller)
+        return seller
 
-    @staticmethod
-    async def create_seller(session: AsyncSession, user_id: int, seller: dict):
-        new_seller = Seller(user_id=user_id, **seller)
-        session.add(new_seller)
-        await session.flush()
-        await session.refresh(new_seller)
-        return new_seller
+    # @staticmethod
+    # async def get_seller_by_id(session: AsyncSession, seller_id: int):
+    #     return await session.scalar(
+    #         select(Seller).where(Seller.seller_id == seller_id)
+    #     )
 
-    @staticmethod
-    async def update_seller(
-        session: AsyncSession, db_seller: Seller, update_data: dict
-    ):
-        for key, value in update_data.items():
-            setattr(db_seller, key, value)
+    # @staticmethod
+    # async def get_sellers(session: AsyncSession):
+    #     result = await session.scalars(select(Seller))
+    #     return result.all()
 
-        session.add(db_seller)
-        await session.flush()
-        await session.refresh(db_seller)
-        return db_seller
+    # @staticmethod
+    # async def create_seller(session: AsyncSession, user_id: int, seller: dict):
+    #     new_seller = Seller(user_id=user_id, **seller)
+    #     session.add(new_seller)
+    #     await session.flush()
+    #     await session.refresh(new_seller)
+    #     return new_seller
+
+    # @staticmethod
+    # async def update_seller(
+    #     session: AsyncSession, db_seller: Seller, update_data: dict
+    # ):
+    #     for key, value in update_data.items():
+    #         setattr(db_seller, key, value)
+
+    #     session.add(db_seller)
+    #     await session.flush()
+    #     await session.refresh(db_seller)
+    #     return db_seller
--- backend/src/router/sellerRouter.py
+++ backend/src/router/sellerRouter.py
@@ -5,15 +5,12 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.database import get_session
+from src.model.seller import Seller
 from src.model.user import User
 from src.repository.sellerRepository import SellerRepository
-from src.schema.messageSchema import Message
 from src.schema.sellerSchema import (
-    SellerCreate,
-    SellerList,
-    SellerPrivate,
     SellerPublic,
-    SellerUpdate,
+    SellerSchema,
 )
 from src.security import get_current_user
 
@@ -22,70 +19,29 @@ Session = Annotated[AsyncSession, Depends(get_session)]
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
-@router.get('/{seller_id}', response_model=SellerPublic)
-async def get_seller_by_id(session: Session, seller_id: int):
-    seller = await SellerRepository.get_seller_by_id(session, seller_id)
-
-    if not seller:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
-    return SellerPublic.model_validate(seller)
-
-
-@router.get('/', response_model=SellerList, status_code=HTTPStatus.OK)
-async def list_sellers(session: Session):
-    sellers = await SellerRepository.get_sellers(session)
-    return {'sellers': sellers}
-
-
-@router.get('/me', response_model=SellerPrivate)
-async def get_my_seller_profile(user: CurrentUser, session: Session):
-    seller_profile = await SellerRepository.get_seller_by_user_id(
+@router.post('/', response_model=SellerPublic, status_code=HTTPStatus.CREATED)
+async def create_seller(
+    seller: SellerSchema, session: Session, user: CurrentUser
+):
+    db_seller = await SellerRepository.get_seller_by_user_id(
         session, user.user_id
     )
-    if not seller_profile:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
 
-    return SellerPrivate.model_validate(seller_profile)
+    if db_seller:
+        raise HTTPException(
+            status_code=HTTPStatus.CONFLICT,
+            detail='Seller profile already exists for this user',
+        )
 
-
-@router.post('/', status_code=HTTPStatus.CREATED, response_model=SellerPrivate)
-async def create_seller_profile(
-    seller: SellerCreate, user: CurrentUser, session: Session
-):
-    return await SellerRepository.create_seller(
-        session, user.user_id, seller.dict()
+    seller = Seller(
+        seller_description=seller.seller_description,
+        seller_bank_account=seller.seller_bank_account,
+        seller_bank_agency=seller.seller_bank_agency,
+        seller_bank_name=seller.seller_bank_name,
+        seller_bank_type=seller.seller_bank_type,
+        seller_status=seller.seller_status,
+        user_id=user.user_id,
     )
 
+    return await SellerRepository.create_seller(session, seller)
 
-@router.patch('/me{seller_id}', response_model=SellerPrivate)
-async def patch_seller(
-    seller_id: int,
-    seller_data: SellerUpdate,
-    user: CurrentUser,
-    session: Session,
-):
-    db_seller = await SellerRepository.get_seller_by_id(
-        session, user.user_id, seller_id
-    )
-    if not db_seller:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
-
-    updated_seller = seller_data.model_dump(exclude_unset=True)
-    return await SellerRepository.update_seller(
-        session, db_seller, updated_seller
-    )
-
-
-@router.delete('/me{seller_id}', response_model=Message)
-async def delete_seller(
-    seller_id: int,
-    user: CurrentUser,
-    session: Session,
-):
-    db_seller = await SellerRepository.get_seller_by_id(
-        session, user.user_id, seller_id
-    )
-    if not db_seller:
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND)
-
-    return await SellerRepository.delete_seller(session, db_seller)
